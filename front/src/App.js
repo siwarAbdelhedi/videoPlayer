4d0c0381372165b3dde9b99eda5d43eb3106bb00
siwarAbdelhedi <abdelhedi.siwar.it@gmail.com>
2024-03-17 15:08:35 +0100

    init

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import VideoPage from './components/videoPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/videoPage" element={<VideoPage/>} />  
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
